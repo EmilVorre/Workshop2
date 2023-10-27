545e4419a25c491c208c2eb1c0777dd5fc203bbc
EmilVorre <10461234@ucn.dk>
2023-10-27 17:01:54 +0200

    update

--- cmake-build-debug/CMakeFiles/3.26.4/CMakeCCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.26.4/CMakeCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_COMPILER "R:/CLion 2023.2.1/bin/mingw/bin/gcc.exe")
+set(CMAKE_C_COMPILER "C:/Users/benth/AppData/Local/JetBrains/CLion 2023.2.1/bin/mingw/bin/gcc.exe")
 set(CMAKE_C_COMPILER_ARG1 "")
 set(CMAKE_C_COMPILER_ID "GNU")
 set(CMAKE_C_COMPILER_VERSION "13.1.0")
@@ -21,11 +21,11 @@ set(CMAKE_C_SIMULATE_VERSION "")
 
 
 
-set(CMAKE_AR "R:/CLion 2023.2.1/bin/mingw/bin/ar.exe")
-set(CMAKE_C_COMPILER_AR "R:/CLion 2023.2.1/bin/mingw/bin/gcc-ar.exe")
-set(CMAKE_RANLIB "R:/CLion 2023.2.1/bin/mingw/bin/ranlib.exe")
-set(CMAKE_C_COMPILER_RANLIB "R:/CLion 2023.2.1/bin/mingw/bin/gcc-ranlib.exe")
-set(CMAKE_LINKER "R:/CLion 2023.2.1/bin/mingw/bin/ld.exe")
+set(CMAKE_AR "C:/Users/benth/AppData/Local/JetBrains/CLion 2023.2.1/bin/mingw/bin/ar.exe")
+set(CMAKE_C_COMPILER_AR "C:/Users/benth/AppData/Local/JetBrains/CLion 2023.2.1/bin/mingw/bin/gcc-ar.exe")
+set(CMAKE_RANLIB "C:/Users/benth/AppData/Local/JetBrains/CLion 2023.2.1/bin/mingw/bin/ranlib.exe")
+set(CMAKE_C_COMPILER_RANLIB "C:/Users/benth/AppData/Local/JetBrains/CLion 2023.2.1/bin/mingw/bin/gcc-ranlib.exe")
+set(CMAKE_LINKER "C:/Users/benth/AppData/Local/JetBrains/CLion 2023.2.1/bin/mingw/bin/ld.exe")
 set(CMAKE_MT "")
 set(CMAKE_COMPILER_IS_GNUCC 1)
 set(CMAKE_C_COMPILER_LOADED 1)
@@ -66,7 +66,7 @@ endif()
 
 
 
-set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "R:/CLion 2023.2.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include;R:/CLion 2023.2.1/bin/mingw/include;R:/CLion 2023.2.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include-fixed;R:/CLion 2023.2.1/bin/mingw/x86_64-w64-mingw32/include")
+set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "C:/Users/benth/AppData/Local/JetBrains/CLion 2023.2.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include;C:/Users/benth/AppData/Local/JetBrains/CLion 2023.2.1/bin/mingw/include;C:/Users/benth/AppData/Local/JetBrains/CLion 2023.2.1/bin/mingw/lib/gcc/x86_64-w64-mingw32/13.1.0/include-fixed;C:/Users/benth/AppData/Local/JetBrains/CLion 2023.2.1/bin/mingw/x86_64-w64-mingw32/include")
 set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "")
 set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "")
 set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
